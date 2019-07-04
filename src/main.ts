56ac5c462db279ac65653787369a1c801752ee08
Andrii Stiahailo <a.stiahailo@keepsolid.com>
2019-07-04 21:01:55 +0300

    fix protocol

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The todo API description')
+    .setSchemes('https', 'http')
     .setVersion('1.0')
     .addTag('todo')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('doc', app, document);
-  app.enableCors();
   await app.listen(process.env.PORT || 5000, '0.0.0.0');
 }
 bootstrap();
