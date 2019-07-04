e378e179a2ac3f02879b733b96f97d1505567ac2
Andrii Stiahailo <a.stiahailo@keepsolid.com>
2019-07-04 19:32:09 +0300

    doing it live

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('doc', app, document);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
